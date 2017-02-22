1818b6efa3ca7481a5e883ae5a67b69478e73a1a
wayne <waynechen@eztravel.com.tw>
2017-02-22 17:12:31 +0800

    檔案整理

--- js/firebase_init.js
+++ js/firebase_init.js
@@ -21,7 +21,7 @@
     })
   }
   function 登入(email,password){
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMsg = error.message;
