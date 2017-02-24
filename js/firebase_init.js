90c6e7cbff630966b742422692f5d8bc789b5c53
wayne <waynechen@eztravel.com.tw>
2017-02-24 15:28:16 +0800

    更新程式

--- js/firebase_init.js
+++ js/firebase_init.js
@@ -4,13 +4,8 @@
 //https://firebase.google.com/docs/auth/web/manage-users  管理使用者帳戶
 
 //https://phpwolf.blogspot.tw/2017/01/firebase-facebook.html   FB	login教學
-//使用Popup註冊FB方式//FB網址請用英文http://localhost/　
 
-
-//更新照片
-//設定email
 //重新驗証用戶 (參考：http://sj82516-blog.logdown.com/posts/1050619)
-//...更多請參考網址
 
 
 
@@ -25,69 +20,56 @@
   firebase.initializeApp(config);
   var database = firebase.database();
 
-  // Firebase 註冊登入
-  
   function 註冊(email, password){
     //如果註冊新帳戶，也會自動登入
     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
       var errorCode = error.code;
       var errorMsg = error.message;
-      console.log(errorMsg);
+      print(errorMsg);
     })
   }
   function 登入(email,password){
     firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
     var errorCode = error.code;
     var errorMsg = error.message;
-    console.log(errorMsg+"___");
+    print(errorMsg+"___");
   	});
   }
   function 登出(){
     firebase.auth().signOut().then(function() {
-      console.log("User sign out!");
+      print("User sign out!");
     }, function(error) {
-      console.log("User sign out error!");
+      print("User sign out error!");
     })
   }
-  function 取得使用者登入狀態(){
-    log(firebase.auth().currentUser);
-  }
-  function 取得使用者登入資料(){
+  function 取得使用者資料(){//頂層
     var user = firebase.auth().currentUser;
-    var name, email, photoUrl, uid, emailVerified;
-
     if (user != null) {
-      name = user.displayName;
-      email = user.email;
-      photoUrl = user.photoURL;
-      emailVerified = user.emailVerified;
-      uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
-                       // this value to authenticate with your backend server, if
-                       // you have one. Use User.getToken() instead.
+      print("使用者名稱: " +user.displayName);
+      print("使用者email: " +user.email);
+      print("使用者照片: " + user.photoURL);
+      print("Email 驗證: " + user.emailVerified);
+      print("uid: " + user.uid);
     }
   }
-  function get_provider_specific(){
-    var user = firebase.auth().currentUser;
+  function 取得使用者登入資料(){
+    var user = firebase.auth().currentUser; //使用者登入狀態
     if (user != null) {
       user.providerData.forEach(function (profile) {
-        console.log("Sign-in provider: "+profile.providerId);
-        console.log("  Provider-specific UID: "+profile.uid);
-        console.log("  Name: "+profile.displayName);
-        console.log("  Email: "+profile.email);
-        console.log("  Photo URL: "+profile.photoURL);
+        print(" Sign-in provider: "+profile.providerId);
+        print("  Provider-specific UID: "+profile.uid);
+        print("  Name: "+profile.displayName);
+        print("  Email: "+profile.email);
+        print("  Photo URL: "+profile.photoURL);
       });
     }
   }
-  function send_verification_email(){
+  function 寄認證信(){
     var user = firebase.auth().currentUser;
     user.sendEmailVerification().then(function() {
-      // Email sent.
-      log("eamil sent")
+      print("eamil sent")
     }, function(error) {
-      // An error happened.
-      log("error")
+      print("error")
     });
   }
   
@@ -95,11 +77,11 @@
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       userLogin = user;
-      console.log("User is logined", user)
+      print("User is logined", user)
 			$("#user").html(user.email)
     } else {
       userLogin = null;
-      console.log("User is not logined yet.");
+      print("User is not logined yet.");
     }
   });
 
@@ -108,8 +90,7 @@
 var provider = new firebase.auth.FacebookAuthProvider();
 
 function fb_login_redirect(){
-	//signInWithPopup
-	//signInWithRedirect
+	//signInWithPopup signInWithRedirect
 	firebase.auth().signInWithPopup(provider).then(function(result) {
 		var token = result.credential.accessToken;
 		var user = result.user;
@@ -121,20 +102,33 @@ function fb_login_redirect(){
 		if (error.code === 'auth/account-exists-with-different-credential') {
 			firebase.auth().fetchProvidersForEmail(email).then(function(providers) {
 				if(providers[0] === "password"){
-					
 					var password = prompt("請輸入email密碼與facebook帳戶綁定");
-				
 					firebase.auth().signInWithEmailAndPassword(email, password).then(function(user) {
             user.link(credential);
           }).then(function() {
-            log("成功連結");
+            print("成功連結");
           });
-					
 				}
 			});
 		}
 	});
 }
+
+//更改基本資料
+function update_Profile(){
+	var user = firebase.auth().currentUser;
+	user.updateProfile({
+		displayName: "wayne",
+		photoURL: "https://example.com/jane-q-user/profile.jpg"
+	}).then(function() {
+		print(user.displayName);
+		print(user.photoURL);
+	}, function(error) {
+		
+	});
+}
+
+
 
 //firebase CRUD 操作  
   function createData(uid, name, email, imageUrl) {
@@ -151,7 +145,7 @@ function fb_login_redirect(){
   var uid="7IJBbfGenZOdVlZjq3nnk1El04T2"
   function postSmtBtn(){
     var postRef = firebase.database().ref('/posts/' + uid);
-   postRef.push().set({
+    postRef.push().set({
       uid: uid,
       title: "g1",
       content: "g2",
@@ -160,6 +154,6 @@ function fb_login_redirect(){
   }
 
 
-function log(a){
+function print(a){
 	console.log(a)
 }
