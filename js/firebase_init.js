51acde9af99c3fe20c75086416f375a52f33d86c
wayne <waynechen@eztravel.com.tw>
2017-02-24 10:54:43 +0800

    fireabse login

--- js/firebase_init.js
+++ js/firebase_init.js
@@ -3,6 +3,10 @@
 //http://sj82516-blog.logdown.com/posts/1050619
 //https://firebase.google.com/docs/auth/web/manage-users  管理使用者帳戶
 
+//https://phpwolf.blogspot.tw/2017/01/firebase-facebook.html   FB	login教學
+//使用Popup註冊FB方式//FB網址請用英文http://localhost/　
+
+
 //更新照片
 //設定email
 //重新驗証用戶 (參考：http://sj82516-blog.logdown.com/posts/1050619)
@@ -47,11 +51,9 @@
       console.log("User sign out error!");
     })
   }
-  
   function 取得使用者登入狀態(){
     log(firebase.auth().currentUser);
   }
-  
   function 取得使用者登入資料(){
     var user = firebase.auth().currentUser;
     var name, email, photoUrl, uid, emailVerified;
@@ -65,9 +67,7 @@
                        // this value to authenticate with your backend server, if
                        // you have one. Use User.getToken() instead.
     }
-
   }
-  
   function get_provider_specific(){
     var user = firebase.auth().currentUser;
     if (user != null) {
@@ -80,7 +80,6 @@
       });
     }
   }
-  
   function send_verification_email(){
     var user = firebase.auth().currentUser;
     user.sendEmailVerification().then(function() {
@@ -103,6 +102,39 @@
       console.log("User is not logined yet.");
     }
   });
+
+
+//firebase to fb login
+var provider = new firebase.auth.FacebookAuthProvider();
+
+function fb_login_redirect(){
+	//signInWithPopup
+	//signInWithRedirect
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+		var token = result.credential.accessToken;
+		var user = result.user;
+	}).catch(function(error) {
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		var email = error.email;
+		var credential = error.credential;
+		if (error.code === 'auth/account-exists-with-different-credential') {
+			firebase.auth().fetchProvidersForEmail(email).then(function(providers) {
+				if(providers[0] === "password"){
+					
+					var password = prompt("請輸入email密碼與facebook帳戶綁定");
+				
+					firebase.auth().signInWithEmailAndPassword(email, password).then(function(user) {
+            user.link(credential);
+          }).then(function() {
+            log("成功連結");
+          });
+					
+				}
+			});
+		}
+	});
+}
 
 //firebase CRUD 操作  
   function createData(uid, name, email, imageUrl) {
@@ -127,80 +159,6 @@
     })
   }
 
-
-//firebase to fb login
-var provider = new firebase.auth.FacebookAuthProvider();
-
-provider.setCustomParameters({
-  'display': 'popup'
-	//page
-});
-//https://phpwolf.blogspot.tw/2017/01/firebase-facebook.html   FB教學
-
-
-//使用Popup註冊FB方式//FB網址請用英文http://localhost/　
-var fbLoginBtn = document.getElementById("fbLoginBtn");
-fbLoginBtn.addEventListener("click",function(){
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    // 取得FB Token，可以使用於FB API中
-    var token = result.credential.accessToken;
-    // 使用者資料
-    var FBUser = result.user;
-    console.log(FBUser);
-  }).catch(function(error) {
-    //處理 帳號已經存在於其他登入方式時
-    if (error.code === 'auth/account-exists-with-different-credential') {
-      //取得credential
-      var pendingCred = error.credential;
-      // The provider account's email address.
-      var email = error.email;
-      console.log("FB登入錯誤-使用者信箱：",email);
-      // 取得當初此Email的登入方式
-      firebase.auth().fetchProvidersForEmail(email).then(function(providers) {
-        // 如果使用者有多個登入方式的話
-        console.log("FB登入錯誤-其他登入方式：",providers);
-        if (providers[0] === 'password') {
-          // 如果使用者用密碼登入，要求使用者輸入密碼
-          var password = promptUserForPassword(); // TODO: 實作 promptUserForPassword.
-          firebase.auth().signInWithEmailAndPassword(email, password).then(function(user) {
-            // Step 4a.
-            return user.link(pendingCred);
-          }).then(function() {
-            // 成功連結
-            goToApp();
-          });
-          return;
-        }
-        // 如果是其他登入方式，必須取得該登入方式,同時提供相對應的Provider
-        // TODO: implement getProviderForProviderId.
-        var provider = getProviderForProviderId(providers[0]);
-        // 此時你必須讓使用者了解到 他曾經用其他方式登入過
-        // Note: 瀏覽器通常會擋住跳出頁面，所以在現實狀況下，最好有個"請繼續"按鈕觸發新的註冊頁面
-        // 可以參考 https://fir-ui-demo-84a6c.firebaseapp.com/
-        auth.signInWithPopup(provider).then(function(result) {
-          // 如果使用者用不同Email登入同一個帳號，這樣Firebase是無法擋住的
-          // Step 4b.
-          // 連結回原本的credential
-          result.user.link(pendingCred).then(function() {
-            // 成功連結
-            goToApp();
-          });
-        });
-      });
-    }
-  });
-},false);
-
-function promptUserForPassword(){
-	var pwd = prompt("Please enter your password");
-    if (pwd != null) {
-    	return pwd;
-    }
-}
-
-function goToApp(){
-  alert("d")
-}
 
 function log(a){
 	console.log(a)
