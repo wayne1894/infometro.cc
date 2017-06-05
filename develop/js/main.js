280b010af08f018423c95c0ab0473b81e6d2d272
wayne <waynechen@eztravel.com.tw>
2017-06-05 15:18:17 +0800

    parse_url

--- develop/js/main.js
+++ develop/js/main.js
@@ -167,92 +167,99 @@
 
   function parse_url(url, fn) {
     $.get("https://infometro.hopto.org/infometro.asp?url=" + url, function (html) {
-      var iframe = document.createElement("iframe");
-      iframe.id = "iframe";
-      iframe.style.display = "none";
-      $(document.body).append(iframe);
-      var ifrm = document.getElementById('iframe');
-      ifrm = ifrm.contentWindow || ifrm.contentDocument.document || ifrm.contentDocument;
-      ifrm.document.open();
-      ifrm.document.write(html);
-      ifrm.document.close();
-      var url_info = {}
-      var metas = $("#iframe")[0].contentWindow.document.getElementsByTagName('meta');
-      for (var i = 0; i < metas.length; i++) {
-        if (metas[i].getAttribute("name") == "description") {
-          url_info.description = metas[i].getAttribute("content");
-        } else if (metas[i].getAttribute("property") == "og:description") {
-          url_info.og_description = metas[i].getAttribute("content");
-        } else if (metas[i].getAttribute("property") == "og:image") {
-          url_info.og_image = metas[i].getAttribute("content").split(",")[0];
-        } else if (metas[i].getAttribute("property") == "og:title") {
-          url_info.og_title = metas[i].getAttribute("content");
-        }
-      }
+		try{
+				var iframe = document.createElement("iframe");
+				iframe.id = "iframe";
+				iframe.style.display = "none";
+				$(document.body).append(iframe);
+				var ifrm = document.getElementById('iframe');
+				ifrm = ifrm.contentWindow || ifrm.contentDocument.document || ifrm.contentDocument;
+				ifrm.document.open();
+				ifrm.document.write(html);
+				ifrm.document.close();
+				var url_info = {}
+				var metas = $("#iframe")[0].contentWindow.document.getElementsByTagName('meta');
+				for (var i = 0; i < metas.length; i++) {
+					if (metas[i].getAttribute("name") == "description") {
+						url_info.description = metas[i].getAttribute("content");
+					} else if (metas[i].getAttribute("property") == "og:description") {
+						url_info.og_description = metas[i].getAttribute("content");
+					} else if (metas[i].getAttribute("property") == "og:image") {
+						url_info.og_image = metas[i].getAttribute("content").split(",")[0];
+					} else if (metas[i].getAttribute("property") == "og:title") {
+						url_info.og_title = metas[i].getAttribute("content");
+					}
+				}
 
-      //字串手動劫取
-      if (url_info.og_image == undefined) { //取fb images
-        if (html.indexOf("og:image") > -1) {
-          var og_html = html.split("og:image")[1].split(">")[0];
-          og_html = og_html.replace(/\'/gi, "\"");
-          og_html = og_html.split("content=\"")[1].split('"')[0]
-          url_info.og_image = og_html;
-        }
-      }
-      if (url_info.og_description == undefined) {
-        if (html.indexOf("og:description") > -1) {
-          var og_html = html.split("og:description")[1].split(">")[0];
-          og_html = og_html.replace(/\'/gi, "\"");
-          og_html = og_html.split("content=\"")[1].split('"')[0]
-          url_info.og_description = og_html;
-        }
-      }
-      if (url_info.og_title == undefined) {
-        if (html.indexOf("og:title") > -1) {
-          var og_html = html.split("og:title")[1].split(">")[0];
-          og_html = og_html.replace(/\'/gi, "\"");
-          og_html = og_html.split("content=\"")[1].split('"')[0]
-          url_info.og_title = og_html;
-        }
-      }
+				//字串手動劫取
+				if (url_info.og_image == undefined) { //取fb images
+					if (html.indexOf("og:image") > -1) {
+						var og_html = html.split("og:image")[1].split(">")[0];
+						og_html = og_html.replace(/\'/gi, "\"");
+						og_html = og_html.split("content=\"")[1].split('"')[0]
+						url_info.og_image = og_html;
+					}
+				}
+				if (url_info.og_description == undefined) {
+					if (html.indexOf("og:description") > -1) {
+						var og_html = html.split("og:description")[1].split(">")[0];
+						og_html = og_html.replace(/\'/gi, "\"");
+						og_html = og_html.split("content=\"")[1].split('"')[0]
+						url_info.og_description = og_html;
+					}
+				}
+				if (url_info.og_title == undefined) {
+					if (html.indexOf("og:title") > -1) {
+						var og_html = html.split("og:title")[1].split(">")[0];
+						og_html = og_html.replace(/\'/gi, "\"");
+						og_html = og_html.split("content=\"")[1].split('"')[0]
+						url_info.og_title = og_html;
+					}
+				}
 
 
-      //var $iframe_body=$(document.getElementById('iframe').contentWindow.document.body);
+				//var $iframe_body=$(document.getElementById('iframe').contentWindow.document.body);
 
-      if (url_info.og_title) {
-        url_info.title = url_info.og_title;
-      } else {
-        url_info.title = $(document.getElementById('iframe').contentWindow.document).find("title").html();
-        if (url_info.title == undefined) url_info.title = "";
-      }
-      if (url_info.og_description) {
-        url_info.description = url_info.og_description;
-      }
-      if (url_info.og_image) {
-        url_info.image = url_info.og_image;
-      }
+				if (url_info.og_title) {
+					url_info.title = url_info.og_title;
+				} else {
+					url_info.title = $(document.getElementById('iframe').contentWindow.document).find("title").html();
+					if (url_info.title == undefined) url_info.title = "";
+				}
+				if (url_info.og_description) {
+					url_info.description = url_info.og_description;
+				}
+				if (url_info.og_image) {
+					url_info.image = url_info.og_image;
+				}
 
-      delete url_info.og_title
-      delete url_info.og_description
-      delete url_info.og_image
+				delete url_info.og_title
+				delete url_info.og_description
+				delete url_info.og_image
 
-      url_info.url = url; //這個url代表是連結的url
-      url_info.url_parent = url.split("://")[1].split("/")[0];
+				url_info.url = url; //這個url代表是連結的url
+				url_info.url_parent = url.split("://")[1].split("/")[0];
 
-      //判斷是不是youtube
-      if (url.indexOf(".youtube.") > -1) {
-        url_info.youtube = url.split("?v=")[1].split("&")[0];
-      } else if (url.indexOf("youtu.be/") > -1) {
-        url_info.youtube = url.split("be/")[1];
-      }
-//      url_info.ico = "https://www.google.com/s2/favicons?domain_url=" + url; 減少流量這個不存了
-      $("#iframe").remove();
-      if (typeof fn === "function") fn(url_info);
+				//判斷是不是youtube
+				if (url.indexOf(".youtube.") > -1) {
+					url_info.youtube = url.split("?v=")[1].split("&")[0];
+				} else if (url.indexOf("youtu.be/") > -1) {
+					url_info.youtube = url.split("be/")[1];
+				}
+	//      url_info.ico = "https://www.google.com/s2/favicons?domain_url=" + url; 減少流量這個不存了
+
+
+				$("#iframe").remove();
+			  if (typeof fn === "function") fn(url_info);
+			}catch(err) {
+				print("網址解析有錯")
+				if (typeof fn === "function") fn();
+			}
 
       //console.log(url_info);
     }).fail(function(){
       if (typeof fn === "function") fn();
-	})
+		})
   }
   function auto_height(textarea){
     $(textarea).height(70);
@@ -280,7 +287,7 @@
       }
   }
   function start_set(){
-      if($.cookie("start")=="Y" || 1==2){//代表第一次進來
+    if($.cookie("start")=="Y" || 1==2){//代表第一次進來
           $("#edit_parent").append("<div class='navOne ui left pointing basic label'>第一次進來嗎？點擊這裡開始導覽。<i class='delete icon' style='float:right'></i></div>");
 
           $("#board_send_parent").append("<div class='navOne ui top pointing basic label'>這個區塊可以新增資訊<i class='delete icon' style='float:right'></i></div>");
