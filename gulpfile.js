2a67090591b6f76b39372af7ddd852bfe9979284
wayne <mose286778@gmail.com>
2017-10-06 17:51:04 +0800

    gulp 整理

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp')
 var gulpPlumber = require('gulp-plumber');// 錯誤處理
+var merge= require('merge-stream');// merge-stream
 
 //gulp-webserver
 var webserver = require('gulp-webserver');
@@ -15,12 +16,11 @@ gulp.task('webserver', function() {
 
 //gulp-less
 var less = require('gulp-less');
-var cssmin = require('gulp-minify-css');
+
 gulp.task('less', function () {
   return gulp.src('develop/css/*.less')
   .pipe(gulpPlumber())
   .pipe(less())
-  .pipe(cssmin())
   .pipe(gulp.dest('./public/src/build/css'));
 });
 
@@ -36,25 +36,52 @@ gulp.task('fileinclude', function() {
 	.pipe(gulp.dest('./public'));
 });
 
-// gulp-uglify
-var gulpUglify = require('gulp-uglify'); 
 // gulp-concatnpm 
 var concat= require('gulp-concat');
-// merge-stream
-var merge= require('merge-stream');
 
-gulp.task('concat', function() { //合併與最小化檔案
+
+gulp.task('concat', function() { //合併檔案
 	var concat1=gulp.src(['develop/js/main.js', 'develop/js/top.js', 'develop/js/bottom.js','develop/js/left.js','develop/js/right.js','develop/js/center.js','develop/js/vm.js'])
-        .pipe(gulpPlumber())
-        .pipe(concat('main.js'))
-		.pipe(gulpUglify())
+		.pipe(gulpPlumber())
+		.pipe(concat('main.js'))
 		.pipe(gulp.dest('./public/src/build/js'));
 	var concat2= gulp.src(['develop/js/firebase_init.js'])
 		.pipe(gulpPlumber())
-		.pipe(gulpUglify())
 		.pipe(gulp.dest('./public/src/build/js'));
 	return merge(concat1, concat2);
 });
+
+var imagemin = require('gulp-imagemin');
+gulp.task('imagemin',function(){
+    gulp.src(['develop/images/*','develop/images/*/*'])
+		.pipe(imagemin())
+		.pipe(gulp.dest('./public/src/build/images'));
+});
+
+
+var staticHash = require('gulp-static-hash');//gulp-static-hash (亂數 hash)
+var htmlmin = require('gulp-htmlmin'); // gulp-htmlmin (最小化檔案)
+var cssmin = require('gulp-minify-css'); //(最小化檔案)
+var gulpUglify = require('gulp-uglify'); // gulp-uglify (最小化檔案)
+gulp.task('html', function () {
+    gulp.src('public/*.html')
+		.pipe(staticHash({asset: 'static'}))
+	  .pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('./public'));
+});
+gulp.task('cssmin', function () {
+    gulp.src('public/src/build/css/*.css')
+		.pipe(cssmin())
+		.pipe(gulp.dest('./public/src/build/css'));
+});
+gulp.task('gulpUglify', function () {
+    gulp.src('public/src/build/js/*.js')
+		.pipe(gulpUglify())
+		.pipe(gulp.dest('./public/src/build/js'));
+});
+
+
+
 
 
 //即時監控
@@ -64,8 +91,9 @@ gulp.task('watch', function () {
 	gulp.watch(['develop/include/*.html'], ['fileinclude']);
 	gulp.watch(['develop/css/*.less'], ['less']);
 	gulp.watch(['develop/js/*.js'], ['concat']);
+	gulp.watch(['develop/images'], ['imagemin']);
 })
 
 
 //預設執行
-gulp.task('default', ['watch','fileinclude', 'less', 'webserver','concat']);
+gulp.task('default', ['watch','fileinclude','imagemin', 'less', 'webserver','concat']);
